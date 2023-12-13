13979e30d9212e5a7e80c819905efd700c0058a6
nsultana5 <98044004+nsultana5@users.noreply.github.com>
2023-12-13 09:39:56 -0500

    Add files via upload

---
+++ vowel_consonent.py
@@ -0,0 +1,11 @@
+                                     #Assignment 3:Write a Python program that takes a single character as input and determines whether it is a vowel or a consonant.
+
+input_char = input("Enter a single character:")
+def vowel_consonant_classify(char):
+    vowels="aeiouAEIOU"
+    if char in vowels:
+        print("It is a vowel.")
+    else:
+        print("It is a consonant.")
+
+output = vowel_consonant_classify(input_char)
