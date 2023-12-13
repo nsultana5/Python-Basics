cc8b478fc319ab764a392123d684516d8ad3a63e
nsultana5 <98044004+nsultana5@users.noreply.github.com>
2023-12-13 09:38:05 -0500

    Add files via upload

---
+++ leap_year_check.py
@@ -0,0 +1,22 @@
+#Condition for leap year
+# 1.A year is a leap year if it is divisible by 400 2.A year is a leap year if it is divisible by 4 but not devisible by 100.
+input_year =int(input("Enter a year:"))                      #user input
+
+def is_leap(year):                                           #define a function called is_leap.
+    if (year %400 ==0) or(year %4 ==0 and year %100 !=0):
+        return True                                          #if block
+    return False                                             #else block
+
+result=is_leap(input_year)
+if result:
+    print("This is a leap year")
+else:
+    print("This is not a leap year")
+
+
+    
+
+
+    
+
+           
